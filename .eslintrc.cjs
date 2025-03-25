e939189e2454ba69c94e4a795a4f5616bad965c7
Thiago <thiagolduarte121@gmail.com>
2025-03-25 17:56:05 -0300

    Updates

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+export default {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+
+    'plugin:react-hooks/recommended',
+
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'react-refresh', '@typescript-eslint'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport:
+ true },
+    ],
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: "/",
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
